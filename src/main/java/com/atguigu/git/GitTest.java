3f11b8fdb97801cb5d46161c89e395c6d5c89bf8
Eric <123@qq.com>
2022-08-22 20:16:37 +0800

    hot_fix first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Idea");
+        System.out.println("Hello Eric");
+
     }
 }
