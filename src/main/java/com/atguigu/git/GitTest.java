d06a97322da3b3db7e92f46af6a2babbd7c47581
Eric <123@qq.com>
2022-08-22 16:18:13 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author Eric
+ * @version 2021.3
+ * @date 2022/8/22 16:14
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
