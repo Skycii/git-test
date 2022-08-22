35102a1186785a6875e127a85b58049b83ca2f65
Eric <123@qq.com>
2022-08-22 20:20:42 +0800

    forth commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,7 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello Idea");
         System.out.println("Hello JAVA");
+        System.out.println("master test");
+
     }
 }
